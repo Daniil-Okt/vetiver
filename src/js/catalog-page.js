52f6d728aa4f2b3bd9c927da6ebc5b15ccb82d32
Daniil-Okt <danikoktysyk@gmail.com>
2025-01-30 19:58:55 +0300

    feat: fix quest block

--- src/js/catalog-page.js
+++ src/js/catalog-page.js
@@ -17,3 +17,11 @@ filterFunction()
 const inputsAutoresize = document.querySelectorAll('input.autoresize');
 setupAutoResize(inputsAutoresize);
 
+
+/* Динамический класса _active элементу при клике ========================================================
+	* Вызвать функцию и передать в нее массив нужных элементов
+	* При клике на элемент, у всех элементов класс удаляется
+*/
+import { toggleActiveClass } from './modules/index.js'
+const questItem = document.querySelectorAll('.quest-item');
+toggleActiveClass(questItem)
